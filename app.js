972af4d1c12f9db7cfe5125ee05aed8a423776fc
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-26 14:33:39 -0400

    Set up model

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override');
+var db = require('./db');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Planning-Poker');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 require('dotenv').config();
-
+var authors = require('./routes/authors');
 var app = express();
 
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,13 +22,15 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/authors', authors);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method'));
 app.use('/', index);
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
