1ff21a9503d37974eae42291fcb84bb5e0244fea
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-28 16:01:28 -0400

    updated show route

--- routes/authors.js
+++ routes/authors.js
@@ -66,79 +66,7 @@ router.post('/', function (request, response) {
 
 });
 
-// show author
-router.get('/:id', function(req, res) {
-  Author.findById(req.params.id)
-    .exec(function(err, author) {
-      if (err) {
-        console.log(err);
-        return;
-      }
 
-      console.log(author);
-      // res.send(author);
-      res.render('authors/show', {
-        author: author
-      });
-    });
-});
-// create author
-router.post('/', function(req, res) {
-  var author = new Author({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    country: req.body.country,
-    book_title: req.body.book_title,
-    publication_year: req.body.publication_year
-  });
-  author.save(function(err, author){
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-     console.log(author);
-    // res.send(author);
-    res.render('author/show', {
-      author: author
-    });
-  });
-});
-// delete author
-router.delete('/:id', function(req, res) {
-  Author.findByIdAndRemove(req.params.id)
-    .exec(function(err, author) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log('Author deleted.');
-      res.send('Author deleted.');
-    });
-});
-// update author
-router.patch('/:id', function(req, res) {
-  Author.findByIdAndUpdate(req.params.id, {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    country: req.body.country,
-    book_title: req.body.book_title,
-    publication_year: req.body.publication_year
-  }, { new: true })
-    .exec(function(err, author) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      console.log(author);
-      // res.send(author);
-      res.render('authors/show', {
-        author: author
-      });
-    });
-});
 
 
 module.exports = router;
