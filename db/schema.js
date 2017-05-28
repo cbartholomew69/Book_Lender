ffd4595cd056993ba4e6f5983c5f6ecd2c04b117
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-05-28 16:39:22 -0400

    updated routes and db folder

--- db/schema.js
+++ db/schema.js
@@ -1,25 +1,36 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// Use native promises
 mongoose.Promise = global.Promise;
 
-var UserSchema = new Schema({
-  email: String,
-  password_digest: String,
-  created_at: Date,
-  updated_at: Date
+var ItemSchema = new Schema({
+  name: String
 });
 
-UserSchema.pre('save', function(next) {
+var UserSchema = new Schema({
+  first_name: String,
+  last_name: String,
+  email: { type: String, required: true, unique: true },
+  created_at: Date,
+  updated_at: Date,
+  items: [ItemSchema]
+});
+
+UserSchema.pre('save', function(next){
   now = new Date();
   this.updated_at = now;
-
-  if (!this.created_at) { this.created_at = now; }
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
   next();
 });
 
-var UserModel = mongoose.model('User', UserSchema);
+
+var UserModel = mongoose.model("User", UserSchema);
+var ItemModel = mongoose.model("Item", ItemSchema);
 
 module.exports = {
-  User: UserModel
-}
+  User: UserModel,
+  Item: ItemModel
+};
