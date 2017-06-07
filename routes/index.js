1325deff54082b1ea671b36e962f4e98da668d6d
cbartholomew69 <bartholomew_christy@yahoo.com>
2017-06-07 12:01:43 -0400

    updated index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Book = require('../models/book');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('books/index', { title: 'Book Lender' });
+  // res.render('index', { title: 'Book Lender' });
+  Book.find({})
+        .exec(function(err, books) {
+            if(err) console.log(err);
+
+            console.log(books);
+            // res.send(books);
+            res.render('books/index', {
+                  books: books
+            });
+        });
 });
 
 module.exports = router;
